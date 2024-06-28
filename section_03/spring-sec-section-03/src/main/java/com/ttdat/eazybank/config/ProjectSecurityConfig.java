c1c608007836ce3ea0fe12be45cde6c9bae45a73
tangthiendat <thiendattang@gmail.com>
2024-06-28 10:12:27 +0700

    Use JdbcUserDetailsManager to perform authentication

--- section_03/spring-sec-section-03/src/main/java/com/ttdat/eazybank/config/ProjectSecurityConfig.java
+++ section_03/spring-sec-section-03/src/main/java/com/ttdat/eazybank/config/ProjectSecurityConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -24,20 +25,26 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails admin = User
+//                .withUsername("admin")
+//                .password("123456")
+//                .authorities("ADMIN")
+//                .build();
+//        UserDetails user = User
+//                .withUsername("user")
+//                .password("123456")
+//                .authorities("READONLY")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails admin = User
-                .withUsername("admin")
-                .password("123456")
-                .authorities("ADMIN")
-                .build();
-        UserDetails user = User
-                .withUsername("user")
-                .password("123456")
-                .authorities("READONLY")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
