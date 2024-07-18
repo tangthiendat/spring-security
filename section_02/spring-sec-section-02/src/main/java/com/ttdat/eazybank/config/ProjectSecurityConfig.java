1dade2fef773627ab6db452c91559f4221300db8
tangthiendat <thiendattang@gmail.com>
2024-07-18 15:21:25 +0700

    Disable formLogin and HttpBasic

--- section_02/spring-sec-section-02/src/main/java/com/ttdat/eazybank/config/ProjectSecurityConfig.java
+++ section_02/spring-sec-section-02/src/main/java/com/ttdat/eazybank/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +15,8 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests(requests -> requests
                         .requestMatchers("/account", "/balance", "/loans", "/cards").authenticated()
                         .requestMatchers("/contact", "/notices").permitAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
