87a903c5cd38c25056db8c3456f9ee22f8554d86
tangthiendat <thiendattang@gmail.com>
2024-07-19 10:39:41 +0700

    Use CompromisedPasswordChecker

--- section_03/spring-sec-section-03/src/main/java/com/ttdat/eazybank/config/ProjectSecurityConfig.java
+++ section_03/spring-sec-section-03/src/main/java/com/ttdat/eazybank/config/ProjectSecurityConfig.java
@@ -2,12 +2,14 @@ package com.ttdat.eazybank.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -47,5 +49,10 @@ public class ProjectSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
