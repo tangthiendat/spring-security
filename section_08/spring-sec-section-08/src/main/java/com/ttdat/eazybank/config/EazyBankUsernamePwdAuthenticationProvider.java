c217c02a2890a6e1e70b9c538026874ddcf90376
tangthiendat <thiendattang@gmail.com>
2024-07-20 10:52:58 +0700

    Implement and customize Authentication Provider

--- section_08/spring-sec-section-08/src/main/java/com/ttdat/eazybank/config/EazyBankUsernamePwdAuthenticationProvider.java
+++ section_08/spring-sec-section-08/src/main/java/com/ttdat/eazybank/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -1,42 +1,32 @@
 package com.ttdat.eazybank.config;
 
-import com.ttdat.eazybank.model.Customer;
-import com.ttdat.eazybank.repository.CustomerRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
 
-    private final CustomerRepository customerRepository;
+    private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
-        String password = authentication.getCredentials().toString();
-        List<Customer> customers = customerRepository.findByEmail(username);
-        if(!customers.isEmpty()){
-            if(passwordEncoder.matches(password, customers.getFirst().getPwd())){
-                List<GrantedAuthority> authorities =
-                        List.of(new SimpleGrantedAuthority(customers.getFirst().getRole()));
-                return new UsernamePasswordAuthenticationToken(username, password,authorities);
-            } else {
-                throw new BadCredentialsException("Invalid password");
-            }
+        String pwd = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(pwd, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(userDetails, pwd, userDetails.getAuthorities());
         } else {
-            throw new BadCredentialsException("User not found");
+            throw new BadCredentialsException("Invalid username or password");
         }
     }
 
--- section_10/spring-sec-section-10/src/main/java/com/ttdat/eazybank/config/EazyBankUsernamePwdAuthenticationProvider.java
+++ section_10/spring-sec-section-10/src/main/java/com/ttdat/eazybank/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -1,51 +1,33 @@
 package com.ttdat.eazybank.config;
 
-import com.ttdat.eazybank.model.Authority;
-import com.ttdat.eazybank.model.Customer;
-import com.ttdat.eazybank.repository.CustomerRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
 
-    private final CustomerRepository customerRepository;
+    private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
-        String password = authentication.getCredentials().toString();
-        List<Customer> customers = customerRepository.findByEmail(username);
-        if (!customers.isEmpty()) {
-            if (passwordEncoder.matches(password, customers.getFirst().getPwd())) {
-                return new UsernamePasswordAuthenticationToken(username, password,
-                        getGrantedAuthorities(customers.getFirst().getAuthorities()));
-            } else {
-                throw new BadCredentialsException("Invalid password");
-            }
+        String pwd = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(pwd, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(userDetails, pwd, userDetails.getAuthorities());
         } else {
-            throw new BadCredentialsException("User not found");
+            throw new BadCredentialsException("Invalid username or password");
         }
-    }
-
-    private List<GrantedAuthority> getGrantedAuthorities(Set<Authority> authorities) {
-        return authorities.stream()
-                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
-                .collect(Collectors.toList());
     }
 
     @Override
