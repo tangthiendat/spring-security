2c1ede1ec18c4792f93e129739f6e1f6b440c7d7
tangthiendat <thiendattang@gmail.com>
2024-07-20 09:40:48 +0700

    Use Bcrypt encoder with PasswordEncoderFactories

--- section_05/spring-sec-section-05/src/main/java/com/ttdat/eazybank/config/ProjectSecurityConfig.java
+++ section_05/spring-sec-section-05/src/main/java/com/ttdat/eazybank/config/ProjectSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,6 +26,6 @@ public class ProjectSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
