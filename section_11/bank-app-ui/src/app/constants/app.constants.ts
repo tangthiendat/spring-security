4c9648058fc22fbb20dd32ba134959624a8ee05d
tangthiendat <thiendattang@gmail.com>
2024-07-30 20:09:14 +0700

    Config client side to use JWT

---
+++ section_11/bank-app-ui/src/app/constants/app.constants.ts
@@ -0,0 +1,9 @@
+export const AppConstants = {
+  CONTACT_API_URL: '/contact',
+  LOGIN_API_URL: '/user',
+  ACCOUNT_API_URL: '/accounts',
+  BALANCE_API_URL: '/balance',
+  LOANS_API_URL: '/loans',
+  CARDS_API_URL: '/cards',
+  NOTICES_API_URL: '/notices',
+};
